b824018d741f8c68edcdba5e96ff3894521ebcce
Love Billenius <lovebillenius@disroot.org>
2026-02-06 14:30:19 +0000

    Update formula to v2.3.3

--- Formula/lolcatpp.rb
+++ Formula/lolcatpp.rb
@@ -1,15 +1,15 @@
 class Lolcatpp < Formula
   desc "A lolcat reimplementation in C++ -- BLAZINGLY FAST"
   homepage "https://github.com/lolcatpp/lolcatpp"
-  url "https://github.com/lolcatpp/lolcatpp/archive/refs/tags/v2.3.2.tar.gz" # source_url_marker
-  sha256 "52c8762aa1d9f3599a0ac1caf8e5b884a4974b1ee515cb8d9af5a27cdd19aedd" # source_sha_marker
+  url "https://github.com/lolcatpp/lolcatpp/archive/refs/tags/v2.3.3.tar.gz" # source_url_marker
+  sha256 "495ff9e333ef1199111ac6b27a6b8f6130e720f8daf5f42ea9f9ee3dae018f65" # source_sha_marker
   license "BSD-3-Clause"
 
   conflicts_with "lolcat", because: "both install a `lolcat` binary"
 
   resource "binary" do
-    url "https://github.com/lolcatpp/lolcatpp/releases/download/v2.3.2/lolcat-macos-arm64" # binary_url_marker
-    sha256 "446d5b86df3df36c6933474a5893f1f26efcdc9fbb5ca5762d1bd61a125bcf11" # binary_sha_marker
+    url "https://github.com/lolcatpp/lolcatpp/releases/download/v2.3.3/lolcat-macos-arm64" # binary_url_marker
+    sha256 "f5afd5e0cf78d382a88e501a20e88ee3ed549a5de9231f02ec2e761ee859595a" # binary_sha_marker
   end
 
   depends_on "cmake" => :build
